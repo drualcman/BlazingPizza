679a007840b6c677cebf48d486f82543444022ac
Sergi Ortiz Gómez <drualcman@msn.com>
2021-03-18 11:02:30 +0800

    add pizza and toppings model and db context

--- BlazingPizza/Server/Models/PizzaStoreContext.cs
+++ BlazingPizza/Server/Models/PizzaStoreContext.cs
@@ -10,7 +10,16 @@ namespace BlazingPizza.Server.Models
     public class PizzaStoreContext : DbContext
     {
         public DbSet<PizzaSpecial> Specials { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
 
         public PizzaStoreContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PizzaTopping>().HasKey(pt => new { pt.ToppingId, pt.PizzaId });
+            modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(pt => pt.Toppings);
+            modelBuilder.Entity<PizzaTopping>().HasOne(pt => pt.Topping).WithMany();
+        }
     }
 }
