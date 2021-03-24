845eed51d990cdd726c6dfc00f0c2dcbe2c87301
Sergi Ortiz Gómez <drualcman@msn.com>
2021-03-24 10:13:33 +0800

    prepare model with tables to store the order

---
+++ BlazingPizza/Shared/LatLong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Shared
+{
+    public class LatLong
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public LatLong() { }
+        public LatLong(double latitude, double longitude):this()            //call the default constructor to set a default process common always
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+
+        /// <summary>
+        /// Calculate the interpolate between 2 points to show the point where we are
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="proportion"></param>
+        /// <returns></returns>
+        public static LatLong Interpolate(LatLong start, LatLong end, double proportion)
+        {
+            return new LatLong(
+                start.Latitude + (end.Latitude - start.Latitude) * proportion,        //calculate latitude
+                start.Longitude + (end.Longitude - start.Longitude) * proportion      //calculate longitude
+                );
+        }
+    }
+}
---
+++ BlazingPizza/Shared/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Shared
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public Address DeliveryAddress { get; set; }
+        public LatLong DeliveryLocation { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public decimal GetTotalPprice() => Pizzas.Sum(p => p.GetTotalPrice());
+        public string GetFormattedTotalPrice() => GetTotalPprice().ToString("0.00");
+
+    }
+}
