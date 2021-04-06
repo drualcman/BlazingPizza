eb2598e1e88958ed3c24d842db0628b24371344f
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-06 20:05:52 +0800

    add identity server and prepare the configuration

---
+++ BlazingPizza/Server/Controllers/OidcConfigurationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server.Controllers
+{
+    public class OidcConfigurationController : Controller
+    {
+        private readonly ILogger<OidcConfigurationController> Logger;
+        private readonly IClientRequestParametersProvider ClientRequestParametersProvider;
+
+        public OidcConfigurationController(ILogger<OidcConfigurationController> logger, 
+            IClientRequestParametersProvider clientRequestParametersProvider)
+        {
+            Logger = logger;
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+        }
+
+        /// <summary>
+        /// Send all the configuration paramenter to the client to get dynamic
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+        {
+            IDictionary<string, string> Parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+            return Ok(Parameters);
+        }
+    }
+}
