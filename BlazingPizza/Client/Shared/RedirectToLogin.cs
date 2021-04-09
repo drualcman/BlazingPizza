5de099a76d7fab1dccca430e7bc0b10c5c016bfc
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-09 10:13:02 +0800

    add redirect component without user interface

---
+++ BlazingPizza/Client/Shared/RedirectToLogin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Shared
+{
+    public class RedirectToLogin: ComponentBase
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        protected override void OnInitialized()
+        {
+            string EscapedUri = Uri.EscapeDataString(Navigation.Uri);
+            Navigation.NavigateTo($"authentication/login?returnUrl={EscapedUri}");
+        }
+    }
+}
