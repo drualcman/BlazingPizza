ad5a6ef7fe10ad23d10e05368d70751f765287c1
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-14 10:22:10 +0800

    add templates component library

---
+++ BlazingPizza.TemplateComponents/TemplateDialog.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.TemplateComponents
+{
+    partial class TemplateDialog
+    {
+        /// <summary>
+        /// If only have one content to receive must be call this property with this name.
+        /// If you will receive more than one content then name most be descriptive
+        /// </summary>
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool Show { get; set; }
+
+    }
+}
