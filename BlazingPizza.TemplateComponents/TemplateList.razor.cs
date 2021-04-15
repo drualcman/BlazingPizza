ff13d02b81914d0d4892db56acf07febc781d1f8
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-15 09:54:19 +0800

    add component template using generic tipes

---
+++ BlazingPizza.TemplateComponents/TemplateList.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.TemplateComponents
+{
+    partial class TemplateList<TItem> : ComponentBase
+    {
+        [Parameter]
+        public Func<Task<List<TItem>>> Loader { get; set; }
+
+        [Parameter]
+        public RenderFragment Loading { get; set; }
+
+        [Parameter]
+        public RenderFragment Empty { get; set; }
+
+        [Parameter]
+        public RenderFragment<TItem> Item { get; set; }
+
+        [Parameter]
+        public string ListGroupClass { get; set; }
+
+
+        List<TItem> Items;
+
+
+        protected override async Task OnParametersSetAsync()
+        {
+            Items = await Loader();
+
+            RenderTreeBuilder treeBuilder = new RenderTreeBuilder();            
+            if (Loading is null)
+            {
+                treeBuilder.Clear();
+                treeBuilder.AddMarkupContent(0, "<div class=\"loading-bar\"></div>");
+                Loading = builder => 
+                {
+                    builder.OpenComponent(0, typeof(TItem));
+                    builder.AddMarkupContent(0, "<div class=\"loading-bar\"></div>");
+                    builder.CloseComponent();
+                };
+            }
+            if (Empty is null)
+            {
+                treeBuilder.Clear();
+                treeBuilder.AddMarkupContent(0, "<h2>No orders yet!</h2>");
+                treeBuilder.AddMarkupContent(1, "<a href=\"\" class=\"btn btn-success\">Get a pizza!</a>");
+                Empty = builder =>
+                {
+                    builder.OpenComponent(0, typeof(TItem));
+                    builder.AddMarkupContent(0, "<h2>No orders yet!</h2>");
+                    builder.AddMarkupContent(1, "<a href=\"\" class=\"btn btn-success\">Get a pizza!</a>");
+                    builder.CloseComponent();
+                };
+            }
+
+        }
+    }
+}
--- BlazingPizza/Client/Pages/Myorders.razor.cs
+++ BlazingPizza/Client/Pages/Myorders.razor.cs
@@ -12,54 +12,69 @@ using System.Threading.Tasks;
 
 namespace BlazingPizza.Client.Pages
 {
-    public partial class Myorders: IDisposable
+    public partial class Myorders //: IDisposable
     {
         [Inject]
-        public OrdersClient Client { get; set; }
+        public OrdersClient Client { get; set; }        
 
-        List<OrderWithStatus> MyOrdersWithStatus;
-
-        CancellationTokenSource PollingCancellationToken;
-
-        private async void PollForUpdates()
+        private async Task<List<OrderWithStatus>> LoadOrders()
         {
-            PollingCancellationToken = new CancellationTokenSource();
 
-            while (!PollingCancellationToken.IsCancellationRequested)
-            {
-                try
-                {
-                    MyOrdersWithStatus = await Client.GetOrders();
-                    StateHasChanged();                    
-                    int ordersDeliveres = MyOrdersWithStatus.Where(o => o.IsDelivered == true).ToList().Count();
-                    int totalOrders = MyOrdersWithStatus.Count();
-                    if (ordersDeliveres == totalOrders) PollingCancellationToken.Cancel();
-                    else await Task.Delay(5000);                    
-                }
-                catch (AccessTokenNotAvailableException ex)
-                {
-                    PollingCancellationToken.Cancel();
-                    ex.Redirect();
-                }
-                catch (Exception ex)
-                {
-                    PollingCancellationToken.Cancel();
-                    //send to log...
-                    Console.Error.WriteLine(ex.Message);
-                    StateHasChanged();
-                }
-            }
+            List<OrderWithStatus> MyOrdersWithStatus = new List<OrderWithStatus>();
+            //try
+            //{
+            //    MyOrdersWithStatus = await Client.GetOrders();
+            //}
+            //catch (AccessTokenNotAvailableException ex)
+            //{
+            //    ex.Redirect();
+            //}
+            return MyOrdersWithStatus;
         }
 
-        protected override void OnInitialized()
-        {
-            PollingCancellationToken?.Cancel();     //if already enter some times and it's looking then cancel to check the new order
-            PollForUpdates();
-        }
+        //List<OrderWithStatus> MyOrdersWithStatus;
 
-        public void Dispose()
-        {
-            PollingCancellationToken?.Cancel();
-        }
+        //CancellationTokenSource PollingCancellationToken;
+
+        //private async void PollForUpdates()
+        //{
+        //    PollingCancellationToken = new CancellationTokenSource();
+
+        //    while (!PollingCancellationToken.IsCancellationRequested)
+        //    {
+        //        try
+        //        {
+        //            MyOrdersWithStatus = await Client.GetOrders();
+        //            StateHasChanged();                    
+        //            int ordersDeliveres = MyOrdersWithStatus.Where(o => o.IsDelivered == true).ToList().Count();
+        //            int totalOrders = MyOrdersWithStatus.Count();
+        //            if (ordersDeliveres == totalOrders) PollingCancellationToken.Cancel();
+        //            else await Task.Delay(5000);                    
+        //        }
+        //        catch (AccessTokenNotAvailableException ex)
+        //        {
+        //            PollingCancellationToken.Cancel();
+        //            ex.Redirect();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            PollingCancellationToken.Cancel();
+        //            //send to log...
+        //            Console.Error.WriteLine(ex.Message);
+        //            StateHasChanged();
+        //        }
+        //    }
+        //}
+
+        //protected override void OnInitialized()
+        //{
+        //    PollingCancellationToken?.Cancel();     //if already enter some times and it's looking then cancel to check the new order
+        //    PollForUpdates();
+        //}
+
+        //public void Dispose()
+        //{
+        //    PollingCancellationToken?.Cancel();
+        //}
     }
 }
