dd9d97ec86ca63e9f81f774195af5992f6b3bd1d
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-20 08:49:17 +0800

    prepare push notifications in a db and the api

---
+++ BlazingPizza/Server/Controllers/NotificationsController.cs
@@ -0,0 +1,44 @@
+﻿using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("notifications")]
+    [ApiController]
+    [Authorize]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly PizzaStoreContext Context;
+
+        public NotificationsController(PizzaStoreContext context)
+        {
+            this.Context = context;
+        }
+
+        [HttpPut("subscribe")]
+        public async Task<NotificationSubscription> Subscribe(NotificationSubscription subscription)
+        {
+            string UserId = GetUserId();
+            IQueryable<NotificationSubscription> oldSubscriptions = Context.NotificationSubscriptions.Where(e => e.UserId == UserId);
+            Context.NotificationSubscriptions.RemoveRange(oldSubscriptions);
+            subscription.UserId = UserId;
+            Context.NotificationSubscriptions.Attach(subscription);
+            await Context.SaveChangesAsync();
+            return subscription;
+        }
+
+
+        private string GetUserId()
+        {
+            return HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
