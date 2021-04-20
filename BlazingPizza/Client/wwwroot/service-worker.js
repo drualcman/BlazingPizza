baf84283dc4f29031213bcd1412360ea3a85a944
Sergi Ortiz Gómez <drualcman@msn.com>
2021-04-20 09:59:19 +0800

    show notifications

--- BlazingPizza/Client/wwwroot/service-worker.js
+++ BlazingPizza/Client/wwwroot/service-worker.js
@@ -10,3 +10,20 @@ self.addEventListener('fetch', event => {
     //here must be the code to get the data from the browser cache
     return null;            //cheat the browser and avoid off line content and request to the server for the content
 });
+
+self.addEventListener('push', event => {
+    const payload = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification('Blazing Pizza', {
+            body: payload.message,
+            icon: 'images/icon-512.png',
+            ibrate: [100, 50, 100],
+            data: { url: payload.url }
+        })
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
